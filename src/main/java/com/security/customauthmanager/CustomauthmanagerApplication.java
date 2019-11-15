49ad246c7b54dd59f70e50335014d96522ac53e7
milad <milad.yeganeh@gmail.com>
2019-11-15 22:28:15 +0330

    password encoding and migration

--- src/main/java/com/security/customauthmanager/CustomauthmanagerApplication.java
+++ src/main/java/com/security/customauthmanager/CustomauthmanagerApplication.java
@@ -6,24 +6,34 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 
 @SpringBootApplication
 public class CustomauthmanagerApplication {
 
+//    @Bean
+    PasswordEncoder oldPasswordEncoder(){
+        String md5 = "MD5";
+        return new DelegatingPasswordEncoder(md5, Collections.singletonMap(md5, new MessageDigestPasswordEncoder(md5)));
+    }
+
     @Bean
     PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
     @Bean
     CustomUserDetailService customUserDetailService () {
         Collection<UserDetails> user = Arrays.asList(
-            new CustomUserDetails("milad", "password", true, "USER"),
-            new CustomUserDetails("masod", "password", true, "USER", "ADMIN")
+            new CustomUserDetails("milad", oldPasswordEncoder().encode("password"), true, "USER"),
+            new CustomUserDetails("masod", oldPasswordEncoder().encode("password"), true, "USER", "ADMIN")
         );
         return new CustomUserDetailService(user);
     }
--- src/main/java/com/security/customauthmanager/service/CustomUserDetailService.java
+++ src/main/java/com/security/customauthmanager/service/CustomUserDetailService.java
@@ -1,6 +1,11 @@
 package com.security.customauthmanager.service;
 
+import com.security.customauthmanager.config.CustomUserDetails;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
@@ -8,21 +13,35 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class CustomUserDetailService implements UserDetailsService {
+
+public class CustomUserDetailService implements UserDetailsService, UserDetailsPasswordService {
 
     private final Map<String, UserDetails> users = new ConcurrentHashMap<>();
+    private final Logger logger = LoggerFactory.getLogger(CustomUserDetailService.class);
 
     public CustomUserDetailService(Collection<UserDetails> seedUsers) {
         seedUsers.forEach(user -> this.users.put(user.getUsername(), user));
+        this.users.forEach((k, v) -> System.out.println(k + ": " + v.getPassword()));
     }
 
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-
         if (this.users.containsKey(userName)){
             return this.users.get(userName);
         }
 
         throw new UsernameNotFoundException(String.format("Could Not found %s:", userName));
+    }
+
+    @Override
+    public UserDetails updatePassword(UserDetails userDetails, String newPassword) {
+        System.out.println("Prompt to update Password for user: " + userDetails.getUsername() + "to " + newPassword);
+        this.users.put(userDetails.getUsername(), new CustomUserDetails(
+                userDetails.getUsername(),
+                newPassword,
+                userDetails.isEnabled(),
+                userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).toArray(String[]::new)
+        ));
+        return this.loadUserByUsername(userDetails.getUsername());
     }
 }
